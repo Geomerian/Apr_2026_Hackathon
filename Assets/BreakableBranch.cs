12a59d01f851f0d5b4fdc414888e3b9a134b5b7d
AnthonyMartinez1101 <anthonym71604@gmail.com>
2026-04-11 01:15:16 -0700

    Breakable Branches
    "Branches" have a script in which a force can be set. The higher the force, the higher you need to be in order to break the branch with downwards force.

---
+++ Assets/BreakableBranch.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BreakableBranch : MonoBehaviour
+{
+    public float breakForce = 10f; // Force required to break the branch
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the colliding object has a Rigidbody and is not kinematic
+        FallSpeedTracker speed = collision.collider.GetComponent<FallSpeedTracker>();
+        if (!speed) return;
+
+        Debug.Log("Downward Speed: " + speed.GetDownwardSpeed());
+
+        // If the impact force exceeds the break force, destroy the branch
+        if (speed.GetDownwardSpeed() > breakForce)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
